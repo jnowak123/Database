784ff4bc4659b009a0d539e5bd6f98049ba09856
jnowak123 <jacksokoleoko@gmail.com>
2021-01-17 14:13:12 +0100

    working on huffman

--- Python/Moje/Huffman/Decode.py
+++ Python/Moje/Huffman/Decode.py
@@ -4,23 +4,30 @@ class Main:
     def __init__(self, path):
         self.path = path
         self.decode()
+    
+    class Node:
+        def __init__(self, char=None, left=None, right=None):
+            self.left = left
+            self.right = right
+            self.char = char
 
-    def get_key(self, text):
-        dic = {}
-        key = ''
-        i = -1
-        while True:
-            i += 1
-            if key[-1] == '1':
-                key = key[:-1]
-                key[-1] = '1'
-                
-            if text[i] = '0':
-                key = key + '0'
-            else:
-                dic[i+1:i+9] = key
-                key[-1] = '1'
-                i += 8
+    def create_tree(self, text, i, carry):
+        if text[i] == '1':
+            node = self.Node(char = chr(int(text[i+1:i+9], 2)))
+        else:
+            left = create_tree(text, i+1)
+            i = i+1 if tex[i+1] == '0' else i+9
+            right = create_tree(text, i+1)
+            node = self.Node(left = left, right = right)
+            i = i+1 if tex[i+1] == '0' else i+9
+        return node, i
+
+    def remove_padding():
+        pass
+
+    def create_key():
+        pass        
+            
 
     def decode(self):
         filename, dummy_file_extension = os.path.splitext(self.path)
@@ -28,6 +35,8 @@ class Main:
             text = file.read()
             text = ''.join(format(ord(text[i]), '08b') for i in range(len(text)))
 
+            tree, i = self.create_tree(text, 0, False)
             output.write(text)
+            
 
 Main('me.bin')
