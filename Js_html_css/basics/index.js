71b8fddc1d43ea072d1068d4b6c43762d7b6ab68
jacksokoleoko <jacksokoleoko@gmail.com>
2021-06-09 12:00:59 +0200

    adding disableOverlay

--- Js_html_css/basics/index.js
+++ Js_html_css/basics/index.js
@@ -1,7 +1,13 @@
 let btn = document.getElementById('cta-btn')
 let overlay = document.getElementById('overlay')
 
+function disableOverlay(){
+    overlay.style.display = 'none'
+    overlay.classList.remove('animate-overlay')
+}
+
 btn.addEventListener('click', () => {
     overlay.style.display = 'grid';
     overlay.classList.add('animate-overlay');
+    setTimeout(disableOverlay, 2800)
 })
