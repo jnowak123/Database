35fe95041ff3f5c3e63f7cf702706d11cbe930d1
jnowak123 <jacksokoleoko@gmail.com>
2022-12-14 12:04:23 +0100

    matura

--- Matura/2020 Listopad/liczby_szczesliwe.py
+++ Matura/2020 Listopad/liczby_szczesliwe.py
@@ -1,5 +1,24 @@
 with open("dane/dane.txt") as f:
     nums = [x.strip() for x in f]
 
-lucky_nums = [1 for x in range(10000)]
+lucky_nums = [x +1 for x in range(0, 10000, 2)]
+
+def remove_nums(ls, x):
+    ans, count = [], 0
+    for num in ls:
+        count += 1
+        if count == x:
+            count = 0
+        else:
+            ans.append(num)
+    return(ans)
+
+count = 1
+while lucky_nums[-1] != lucky_nums[count]:
+    count += 1
+    lucky_nums = remove_nums(lucky_nums, lucky_nums[count])
+
 print(lucky_nums)
+
+
+
--- Matura/2021 Marzec/wyscigi.sql
+++ Matura/2021 Marzec/wyscigi.sql
@@ -1,7 +1,38 @@
-select imie, nazwisko, czas from czasy c join startujacy s on s.id_startu = c.id_startu join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
-order by czas limit 1;
+select imie, nazwisko, czas 
+from czasy c join startujacy s on c.id_startu = s.id_startu join zawodnicy z on z.id_zawodnika = s.id_zawodnika order by czas limit 1;
 
-select imie, nazwisko, count(id_startu) x from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika
-where obywatel_kraju = 'Polska' group by s.id_zawodnika order by x desc limit 1;
+select imie, nazwisko, count(*) c from startujacy s join zawodnicy z on s.id_zawodnika = z.id_zawodnika
+where obywatel_kraju = 'POLSKA' group by s.id_zawodnika order by c desc limit 1;
 
-select *, (rok - year(data)) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika order by wiek1;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2008' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2009' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2010' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2011' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2012' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2013' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2014' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2015' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2016' order by wiek;
+select imie, nazwisko, rok - year(data) wiek from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika 
+where rok = '2017' order by wiek;
+
+select rok, count(id_startu) c from startujacy 
+where id_startu not in (select id_startu from czasy) group by rok order by c desc limit 1;
+
+select *, count(distinct obywatel_kraju) c from startujacy group by id_zawodnika order by c desc;
+select id_zawodnika, obywatel_kraju from startujacy where id_zawodnika in (199, 252) group by obywatel_kraju order by id_zawodnika;
+
+select s.rok, imie, nazwisko from startujacy s join zawodnicy z using(id_zawodnika) join
+(select rok, max(year(data)) mrok from startujacy s join zawodnicy z on z.id_zawodnika = s.id_zawodnika group by rok) a
+on a.rok = s.rok and mrok = year(data)
+
+
